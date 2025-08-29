53cbff000e4839392c56d6042b437f8363787441
kristianusnata <kristianusnata@gmail.com>
2025-08-29 13:01:20 +0700

    first commit

---
+++ script/index.js
@@ -0,0 +1,36 @@
+const toggleMenu = document.querySelector(`button.header__navbar-toggler`);
+const navbarCollapse = document.querySelector(`.header__navbar-collapse`);
+const navbarNav = navbarCollapse.querySelector(`ul.header__navbar-nav`);
+const body = document.querySelector(`body`);
+
+toggleMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentTarget = e.currentTarget;
+  const expanded = currentTarget.getAttribute("aria-expanded") === "true";
+
+  // toggle value
+  currentTarget.setAttribute("aria-expanded", expanded ? "false" : "true");
+
+  // delay timer
+  let navbarNavTimerId;
+
+  // control navbarCollapse
+  if (currentTarget.getAttribute("aria-expanded") === "true") {
+    navbarCollapse.classList.add(`header__navbar-collapse--show`);
+
+    if (navbarNavTimerId) clearTimeout(navbarNavTimerId);
+
+    navbarNav.classList.add(`header__navbar-nav--show`);
+    body.style.overflow = "hidden";
+  } else if (currentTarget.getAttribute("aria-expanded") === "false") {
+    navbarNav.classList.remove(`header__navbar-nav--show`);
+
+    if (navbarNavTimerId) clearTimeout(navbarNavTimerId);
+
+    // waiting navbarNav transition to finish
+    navbarNavTimerId = setTimeout(() => {
+      navbarCollapse.classList.remove(`header__navbar-collapse--show`);
+      body.style.overflow = "visible";
+    }, 500);
+  }
+});
